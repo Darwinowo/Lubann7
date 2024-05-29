8c3f42da7df9966d57128c05e42aaa2a591130c4
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-29 09:44:24 -0500

    Actualizar rpg-unreg.js

--- plugins/rpg-unreg.js
+++ plugins/rpg-unreg.js
@@ -1,11 +1,11 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {args}) {
-  if (!args[0]) throw '*⚠️ Ingrese Su Número De Série, Si No Sabe Cual Es Use #myns*';
+  if (!args[0]) throw '👑 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴, 𝚂𝙸 𝙽𝙾 𝚂𝙴 𝙻𝙾 𝚂𝙰𝙱𝙴 𝚄𝚂𝙴: #myns';
   const user = global.db.data.users[m.sender];
   const sn = createHash('md5').update(m.sender).digest('hex');
-  if (args[0] !== sn) throw '*⚠️ Verifique Que Su Número De Série Sea Correcto*';
+  if (args[0] !== sn) throw '🌺 𝙴𝙻 𝙽𝚄𝙼𝙴𝚁𝙾 𝙴𝚂 𝙸𝙽𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾 ❗';
   user.registered = false;
-  m.reply(`✅ *Registro eliminado*`);
+  m.reply(`🌟 𝚈𝙰 𝙽𝙾 𝙴𝚂𝚃𝙰𝚂 𝙴𝙽 𝙼𝙸 𝙱𝙰𝚂𝙴 𝙳𝙴 𝙳𝙰𝚃𝙾𝚂 💫`);
 };
 handler.help = ['', 'ister'].map((v) => 'unreg' + v + ' <numero de serie>');
 handler.tags = ['xp'];
