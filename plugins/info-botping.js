84f703bac4b6a4e05eba2406133487652eba9735
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-05-29 09:13:11 -0500

    Actualizar info-botping.js

--- plugins/info-botping.js
+++ plugins/info-botping.js
@@ -20,7 +20,7 @@ let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
 
-let texto = `╭━〔  𝐘𝐎𝐒𝐇𝐈𝐊𝐎 𝐁𝐎𝐓 🍄  〕⬣
+let texto = `╭━〔  𝐒𝐀𝐊𝐔𝐑𝐀 𝐁𝐎𝐓 🍄  〕⬣
 ┃ *🔮 Velocidad*
 ┃ • ${latensi.toFixed(4)}
 ┃
